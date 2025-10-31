21d470aa1436605fb2503dea906b5414e2c0e4b7
evgenii <evgenii03kroh@gmail.com>
2025-10-31 15:06:25 +0300

    задачи на 31 октября

---
+++ bears.js
@@ -0,0 +1,24 @@
+const animals=[
+    "bears", "wolves", "bears", "wolves", "bears", "wolves", "bears", "wolves", "bears", "wolves",
+    "wolves", "bears", "wolves", "bears", "wolves", "bears", "wolves", "wolves", "bears", "wolves",
+    "bears", "bears", "wolves", "bears", "wolves", "bears", "wolves", "bears", "wolves", "wolves",
+    "bears", "wolves", "bears", "wolves", "wolves", "bears", "bears", "wolves", "wolves", "bears",
+    "wolves", "bears", "wolves", "bears", "wolves", "bears", "wolves", "wolves", "bears", "wolves",
+    "bears", "wolves", "wolves", "bears", "bears", "wolves", "wolves", "wolves", "bears", "bears",
+    "wolves", "bears", "wolves", "bears", "wolves", "wolves", "bears", "wolves", "bears", "wolves",
+    "bears", "wolves", "wolves", "bears", "wolves", "bears", "wolves", "wolves", "bears", "bears",
+    "wolves", "bears", "wolves", "bears", "wolves", "bears", "wolves", "wolves", "bears", "wolves",
+    "wolves", "bears", "wolves", "bears", "wolves", "bears", "wolves", "bears", "wolves", "wolves",
+    "bears", "wolves", "wolves", "bears", "wolves", "bears", "wolves", "bears", "wolves", "bears",
+    "wolves", "wolves", "bears", "bears", "wolves", "bears", "wolves", "wolves", "bears", "wolves",
+    "wolves", "bears", "bears", "wolves", "bears", "wolves", "bears", "wolves", "wolves", "bears",
+    "wolves", "bears", "wolves", "bears", "wolves", "wolves", "bears", "wolves", "bears", "wolves",
+    "wolves", "bears", "bears", "wolves", "wolves", "bears", "wolves", "bears", "wolves", "bears",
+    "wolves", "wolves", "bears", "wolves", "bears", "wolves", "bears", "wolves", "wolves", "bears",
+    "bears", "wolves", "wolves", "bears", "bears", "wolves", "bears", "wolves", "wolves", "bears",
+    "wolves", "bears", "wolves", "bears", "wolves", "bears", "wolves", "bears", "wolves", "wolves",
+    "bears", "wolves", "wolves", "bears", "bears", "wolves", "wolves", "bears", "bears", "wolves"
+]
+console.log (animals.length)
+
+console.log(animals.filter(a=> a==='bears').length)
---
+++ word.js
@@ -0,0 +1,4 @@
+const word='ядернаябомба'
+const firstletter = word.slice(0,1).toUpperCase()
+const otherletter = word.slice(1,word.length)
+console.log (firstletter+otherletter)
